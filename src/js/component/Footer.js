8ad3beba17c6733cbc958984fa25ec4b50a6e98b
Petrement <lspetrement@gmail.com>
2023-04-10 11:22:45 +0000

    last changes

---
+++ src/js/component/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-dark text-white p-3 text-center">
+      Created by <a href="https://www.linkedin.com/in/lola-petrement/" target="_blank" className="text-decoration-none text-warning">Petrement</a>
+    </footer>
+  );
+};
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,37 +1,55 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+//Context
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+//Views
+import { Home } from "../js/views/Home.js";
+import { Films } from "../js/views/Films.js";
+import { FilmsDetails } from "../js/views/FilmsDetails.js";
+import { People } from "../js/views/People.js";
+import { PeopleDetails } from "../js/views/PeopleDetails.js";
+import { Planets } from "../js/views/Planets.js";
+import { PlanetsDetails } from "../js/views/PlanetsDetails.js";
+import { Species } from "../js/views/Species.js";
+import { SpeciesDetails } from "../js/views/SpeciesDetails.js";
+import { Starships } from "../js/views/Starships.js";
+import { StarshipsDetails } from "./views/StarshipsDetails.js";
+import { Vehicles } from "../js/views/Vehicles.js";
+import { VehiclesDetails } from "../js/views/VehiclesDetails.js";
 
-//create your first component
+//Components
+import { Navbar } from "../js/component/Navbar.js";
+import { Footer } from "../js/component/Footer.js";
+
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+  const basename = process.env.BASENAME || "";
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/planets" element={<Planets />} />
+          <Route path="/planets/:planetID" element={<PlanetsDetails />} />
+          <Route path="/characters" element={<People />} />
+          <Route path="/characters/:peopleID" element={<PeopleDetails />} />
+          <Route path="/species" element={<Species />} />
+          <Route path="/species/:specieID" element={<SpeciesDetails />} />
+          <Route path="/starships" element={<Starships />} />
+          <Route path="/starships/:starshipID" element={<StarshipsDetails />} />
+          <Route path="/vehicles" element={<Vehicles />} />
+          <Route path="/vehicles/:vehicleID" element={<VehiclesDetails />} />
+          <Route path="/films" element={<Films />} />
+          <Route path="/films/:filmID" element={<FilmsDetails />} />
+          <Route path="*" element={<h1>Not found!</h1>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
